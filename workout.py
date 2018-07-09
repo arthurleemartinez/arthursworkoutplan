ceed4033414de1922b7f7564571365ac2c1fb659
arthurleemartinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-07-09 04:32:26 -0500

    Update workout.py

--- workout.py
+++ workout.py
@@ -16,31 +16,53 @@ import datetime
 #initialize program and generic user interface
 print "Current date and time: " , datetime.datetime.now()
 user_weight = raw_input(‘Please Enter Your Weight’)
- 
-def sets: 
-if day in daysof_week 
-def random_order_gen:
-	random.choice(group_squared)
- 
+
+#The 3-day group performed one set of each exercise three times a week. 
+sets = 1
+def choose_exercises:
+	if 
+def Day_plan(muscle_areas):
+     for in range(1, 31, [1])
+     rc1 = random.choice(muscle_areas)
+     if rc1 =  rc2 
+	
+daysof_week = {“Sunday” : 0, “Monday” : 1, “Tuesday” : 2, “Wednesday” : 3, “Thursday” : 4, “Friday” : 5, “Saturday” : 6} 
+
 muscle_areas = [“Chest”, “Biceps”, “Abdominals”, “Lats”, “Triceps”, “Forearms”, “Shoulders”, “Middle Back”, “Calves”,  “Hamstrings”,  “Quadriceps”,  “Traps”,  “Lower Back”,  “Glutes”, “Abductors”, “Adductors”]
+
 Chest_exercises: {“Dumbbell Bench Press” : 9.2, ”Pushups” : 9.2, ”Dumbbell Flyes” : 9.1, “Incline Dumbbell Bench Press” : 9.1, “Medium-Grip Barbell Bench Press” : 9.0, “Chest Dips” : 9.0}
+
 Biceps_exercises: {“Incline Hammer Curls” : 9.5, ”Wide-Grip Standing Barbell Curl” : 9.3, ”Spider Curl” : 9.2, ”EZ-Bar Curl” : 9.1, ”Hammer Curls” : 9.1, ”Zottman Curl” : 9.1, ”Concentration Curls” : 9.0, ”Barbell Curl” : 9.0”, “Dumbbell Bicep Curl” : 8.9}
-Abdominals_exercises: {“One-Arm Medicine Ball Slam” : 9.5, ”Landmine 180's” : 9.5, ”Plank” : 9.3, ”Elbow to Knee” : 9.1, ”Plate-Twist” : 9.0, ”Bottom’s Up” : 9.3}
+
+Abdominals_exercises: {“One-Arm Medicine Ball Slam” : 9.5, ”Landmine180s” : 9.5, ”Plank” : 9.3, ”Elbow to Knee” : 9.1, ”Plate-Twist” : 9.0, ”Bottom’s Up” : 9.3}
+
 Lats_exercises: {“Weighted Pull Ups” : 9.5, ”Pullups” : 9.2,  ”Chin-Up” : 9.1, ”Wide-Grip Pull-Up” : 8.9, ”Close-Grip Front Lat Pulldown” : 8.8, ”Shotgun Row” : 8.8}
+
 Triceps_exercises: {“Dips - Triceps Version” : 9.4, ”Decline EZ Bar Triceps Extension” : 9.3, ”Dumbbell Floor Press” : 9.3, ”Close-Grip Barbell Bench Press” : 9.1, ”Triceps Pushdown - V-Bar Attachment” : 9.1, ”Weighted Bench Dip” : 9.1, ”Push-Ups - Close Triceps Position” : 9.0, ”Close-Grip Barbell Bench Press” : 9.1}
+
 Forearms_exercises: {“Seated Palm-Up Barbell Wrist Curl” : 8.7, ”Seated One-Arm Dumbbell Palms-Up Wrist Curl : 9.0” : ”Wrist Roller” : 9.1, ”Palms-Up Barbell Wrist Curl Over A Bench” : 9.4, ”Wrist Rotations with Straight Bar” : 9.5}
+
 Shoulders_exercises: {“Single-Arm Linear Jammer” : 9.5, ”Side Laterals to Front Raise” : 9.5, ”Standing Palm-In One-Arm Dumbbell Press” : 9.4, ”Power Partials” : 9.1,  ”Seated Dumbbell Press” : 9.1, ”Side Laterals to Front Raise” : 9.5, ”Standing Palm-In One-Arm Dumbbell Press” : 9.4, ”One-Arm Side Laterals” : 9.1}
+
 Middle_Back_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Calves_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Hamstrings_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Quadriceps_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Traps_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Lower_Back_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Glutes_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Abductors_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
+
 Adductors_exercises: {“”:””:””:””:””:””:””:””:””:””:””:””:}
  
-daysof_week = {“Sunday” : 0, “Monday” : 1, “Tuesday” : 2, “Wednesday” : 3, “Thursday” : 4, “Friday” : 5, “Saturday” : 6}
+
  
  
 exercise_ratings =  {“Flat Bench”: ::::}Even though the total weekly training volume was the same, it was the guys training each muscle group three times a week who saw the greatest gains in both size and strength.
